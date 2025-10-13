7ef573c194ee4afd51df5e59d74db202737f12cc
renan.arv <renan.santana.arv@gmail.com>
2025-10-13 08:49:47 -0300

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,6 +1,5 @@
 bool BFS(vector<vector<int>>& adj, vector<bool>& mark, int v, int v2) {
     queue<int> queue_visit;
-    vector<int> predecessors(adj.size(), -1);
     queue_visit.push(v);
     mark[v] = true;
     int vbegin = v;
@@ -11,23 +10,6 @@ bool BFS(vector<vector<int>>& adj, vector<bool>& mark, int v, int v2) {
             if(mark[i] == false) {
                 mark[i] = true;
                 queue_visit.push(i);
-                predecessors[i] = v;
-            }
-            if(i == v2) {
-                path.push(v2);
-                while(v2 != vbegin) {
-                    path.push(predecessors[v2]);
-                    v2 = predecessors[v2];
-                }
-                while(path.size() > 0) {
-                    if(path.size() == 1) {
-                        cout << path.top() << "\n";
-                    } else {
-                        cout << path.top() << " ";
-                    }
-                    path.pop();
-                }
-                return true;
             }
         }
     }
