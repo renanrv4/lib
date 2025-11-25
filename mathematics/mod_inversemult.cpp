85be398d4d4cff110a15c658b6445928083f58e7
renanrv4 <renan.santana.arv@gmail.com>
2025-11-25 09:04:58 -0300

    Encontrando o inverso multiplicativo modular de um número resolvendo a identidade de Bézout

---
+++ mathematics/mod_inversemult.cpp
@@ -0,0 +1,16 @@
+int mod_inverse(int a, int m) {
+    int m0 = m;
+    int y = 0, x = 1;
+    while(a > 1) {
+        int q = a / m;
+        int t = m;
+        m = a % m, a = t;
+        t = y;
+        y = x - q * y;
+        x = t;
+    }
+    if(x < 0) {
+        x = x + m0;
+    }
+    return x;
+}
