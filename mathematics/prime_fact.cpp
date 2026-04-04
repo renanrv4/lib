022cffd5079ddc641581e5b0b2cc0e25f3536c7f
renan.arv <renan.santana.arv@gmail.com>
2026-04-04 08:14:14 -0300

    Fix:Prime Factors

--- mathematics/prime_fact.cpp
+++ mathematics/prime_fact.cpp
@@ -1,5 +1,23 @@
 #define ll long long
 
+vector<pair<ll, int>> prime_factors(ll num) {
+    vector<pair<ll, int>> factors;
+    for(ll i = 2; i * i <= num; i++) {
+        int count = 0;
+        while(num % i == 0) {
+            num /= i;
+            count++;
+        }
+        if(count > 0) {
+            factors.push_back({i, count});
+        }
+    }
+    if(num > 1) {
+        factors.push_back({num, 1});
+    }
+    return factors;
+}
+
 vector<ll> prime_factors(ll num) {
     vector<ll> factors;
     ll pf_idx = 0, pf = primes[pf_idx];
