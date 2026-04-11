1059fbd34c8edbb0f0c14ab15e0ab44b8a52cec1
renan.arv <renan.santana.arv@gmail.com>
2026-04-10 21:14:57 -0300

    Add utility functions for geometric calculations

---
+++ geometry/util_functions.cpp
@@ -0,0 +1,8 @@
+double dot(const Point& p, const Point& q) { return p.x*q.x + p.y*q.y; }
+double cross(const Point& p, const Point& q) { return p.x*q.y - p.y*q.x; }
+double norm(const Point& p) { return hypot(p.x, p.y); }
+double dist(const Point& p, const Point& q) { return hypot(p.x-q.x, p.y-q.y); }
+double dist2(const Point& p, const Point& q) { return dot(p-q, p-q); }
+Point normalize(const Point& p) { return p/hypot(p.x, p.y); }
+double angle(const Point& p, Point& q) { return atan2(cross(p, q), dot(p, q)); }
+double angle(const Point& p) { return atan2(p.y, p.x); }
