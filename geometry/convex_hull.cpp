e15f6de1aa8492e92252ae0ec6738586df99ef3e
renan.arv <renan.santana.arv@gmail.com>
2026-04-10 21:17:48 -0300

    Implement isInside function for convex hull
    Add function to check if a point is inside the convex hull

--- geometry/convex_hull.cpp
+++ geometry/convex_hull.cpp
@@ -37,3 +37,24 @@ vector<Point> convexHull(vector<Point>& points, bool sorted = false) {
     lower.insert(lower.end(), upper.begin(), upper.end());
     return lower;
 }
+
+// Checar se o ponto está dentro do hull
+bool isInside(const vector<Point>& hull, Point p) {
+    int n = hull.size();
+    Point v0 = p - hull[0], v1 = hull[1] - hull[0], v2 = hull[n-1] - hull[0];
+    if(cross(v0, v1) > 0 || cross(v0, v2) < 0) {
+        return false;
+    }
+    int l = 1, r = n-1;
+    while(l != r) {
+        int mid = (l + r + 1)/2;
+        Point p0 = p - hull[0], p1 = hull[mid] - hull[0];
+        if(cross(p0, p1) < 0) {
+            l = mid;
+        } else {
+            r = mid - 1;
+        }
+    }
+    v0 = hull[(l+1)%n] - hull[l], v1 = p - hull[l];
+    return cross(v0, v1) >= 0;
+}
