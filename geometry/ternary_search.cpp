eefb3328baadf13c760c16f4fc0c9e62678316f1
renan.arv <renan.santana.arv@gmail.com>
2026-04-28 21:51:15 -0300

    Refactor ternary search and add alternative maximization
    Refactor ternary search implementation to use function F for cost calculations and add an alternative method for maximizing the function.

--- geometry/ternary_search.cpp
+++ geometry/ternary_search.cpp
@@ -1,12 +1,29 @@
-int l = 0, r = n-1;
+int l = 0, r = LIM;
 while(r - l > 5) {
     int m1 = l + (r - l) / 3;
     int m2 = r - (r - l) / 3;
-    double resm1 = costWestEast(west, east, lens, distOAi, m1, j);
-    double resm2 = costWestEast(west, east, lens, distOAi, m2, j);
+    double resm1 = F(m1);
+    double resm2 = F(m2);
     if(resm1 < resm2) {
         r = m2 - 1;
     } else {
         l = m1 + 1;
     }
 }
+
+// Outra forma de fazer busca ternária (maximizando a função também)
+
+double l = 0, r = LIM;
+for(int i = 0; i < 100; i++) {
+    double m1 = l + (r - l) / 3;
+    double m2 = r - (r - l) / 3;
+    double res1 = F(m1);
+    double res2 = F(m2);
+    if(res1 < res2) {
+        l = m1;
+    } else {
+        r = m2;
+    }
+}
+double f = (l+r)/2;
+double res = F(f);
