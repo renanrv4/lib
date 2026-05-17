e21ce8d7b861e35385460fd2f0c393a638439a82
renanrv4 <renan.santana.arv@gmail.com>
2026-05-17 18:16:25 -0300

    Adding an utils archive

---
+++ utils.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define i128 __int128_t
+
+// Usando i128 pra evitar overflow no lcm
+i128 lcmgcd(ll a, ll b) {
+    return (i128)a/gcd(a, b) * b;
+}
+// Dps checa se o lcm é maior que 1e18 e decide o que fazer (ignora ou outra coisa)
+
