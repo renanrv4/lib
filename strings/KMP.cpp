4381383729388a05643ded5ab0917eaee7ca556c
renan.sc <renan.santana.arv@gmail.com>
2026-09-10 15:45:03 -0300

    Refactor KMP algorithm and add Automato structure
    Refactored KMP algorithm implementation with additional functions for pattern matching and state management.

--- strings/KMP.cpp
+++ strings/KMP.cpp
@@ -1,16 +1,45 @@
-int qtd_patterns(vector<int>& seq, vector<int>& org) {
-    vector<int> p = Pi(seq);
-    int qtd = 0;
-    for(int i = 0, j = 0; i < org.size(); i++) {
-        while(j > 0 && org[i] != seq[j]) {
-            j = p[j-1];
-        }
-        if(org[i] == seq[j]) {
-            j++;
-        }
-        if(j == seq.size()) {
-            qtd++; j = p[j-1];
-        }
-    }
-    return qtd;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> Pi(string &t){
+	vector<int> p(t.size(), 0);
+
+	for(int i=1, j=0; i<t.size(); i++){
+		while(j > 0  && t[j] != t[i]) j = p[j-1];
+		if(t[j] == t[i]) j++;
+		p[i] = j;
+	}
+	return p;
 }
+
+vector<int> kmp(string &s, string &t){
+	vector<int> p = Pi(t), occ;
+
+	for(int i=0, j=0; i<s.size(); i++){
+		while( j > 0 && s[i] != t[j]) j = p[j-1];
+		if(s[i]==t[j]) j++;
+		if(j == t.size()) occ.push_back(i-j+1), j = p[j-1];
+	}
+	return occ;
+}
+
+/*BLOCK_DESC_BEGIN **Optional:** KMP Automato. j = state atual [root=j=0] BLOCK_DESC_END*/
+struct Automato {
+	vector<int> p;
+    string t;
+	Automato(string &t) : t(t), p(Pi(t)){}
+    int next(int j, char c){ //return nxt state
+        if(final(j)) j = p[j-1];
+        while(j && c != t[j]) j = p[j-1];
+        return j + (c == t[j]);
+    }
+    bool final(int j){ return j == t.size(); }
+};
+
+/**************************
+**KMP** - Knuth–Morris–Pratt Pattern Searching
+Complexity: O(|S|+|T|)
+kmp(s, t) -> returns all occurences of t in s
+p = Pi(t) -> p[i] = biggest prefix that is a sufix of t[0,i]
+***************************/
